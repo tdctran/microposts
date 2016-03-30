39897361a3e481d8aeb229c62146364b63b3443a
tdctran <tdctran@gmail.com>
2016-03-30 11:43:46 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
