7a366af707e77b5175cc3ce8ed07860b453ac9db
tdctran <tdctran@gmail.com>
2016-03-30 12:50:07 +0000

    Tentative L7 C1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,40 @@ def create
     else
       render 'new'
     end
-  end
+end
+
+def edit
+    @session_id = session[:user_id]
+    if current_user == @user
+      if @user.update(user_params)
+        flash[success] = "プロフィールを編集しました"
+      redirect_to root_path
+      else
+      # 保存に失敗した場合は編集画面へ戻す
+      flash.now[:alert] = "プロフィールの保存に失敗しました"
+      render 'edit'
+      end
+    else
+    flash[:danger] = "正しいユーザー名でログインし直して下さい"
+    redirect_to login_url
+    end
+end
+
+def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'プロフィールを更新しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+end
 
   private
 
-  def user_params
+def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-  end
+end
+
 end
