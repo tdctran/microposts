718f2073f0259ca4192046f436bf006363f8e26b
tdctran <tdctran@gmail.com>
2016-04-01 07:45:08 +0000

    L7 C1 first trial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-
-  def show # 追加
-   @user = User.find(params[:id])
-  end
+  before_action :set_user, only: [:show, :edit, :update]
   
+  def show # 追加
+  end
+
   def new
     @user = User.new
   end
-
-def create
+  
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
@@ -16,26 +16,17 @@ def create
     else
       render 'new'
     end
-end
-
-def edit
+  end
+  
+  def edit
     @session_id = session[:user_id]
-    if current_user == @user
-      if @user.update(user_params)
-        flash[success] = "プロフィールを編集しました"
-      redirect_to root_path
-      else
-      # 保存に失敗した場合は編集画面へ戻す
-      flash.now[:alert] = "プロフィールの保存に失敗しました"
-      render 'edit'
-      end
-    else
-    flash[:danger] = "正しいユーザー名でログインし直して下さい"
-    redirect_to login_url
+    unless current_user == @user
+      flash[:danger] = "正しいユーザー名でログインし直して下さい"
+      redirect_to login_url
     end
-end
-
-def update
+  end
+  
+  def update
     if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
       redirect_to root_path , notice: 'プロフィールを更新しました'
@@ -43,13 +34,16 @@ def update
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
-end
-
+  end
+  
   private
-
-def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
