a2ea85e9b0bbfcf914b64435c863de1b734efee6
tdctran <tdctran@gmail.com>
2016-04-04 10:16:18 +0000

    add followings/followers

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
   resources :microposts
 #  resources :sessions, only: [:new, :create, :destroy]
+ resources :relationships, only: [:create, :destroy]
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
