4520e64096d55bba270d271b03c27e53df242fca
tdctran <tdctran@gmail.com>
2016-04-04 09:08:58 +0000

    20160404

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'signup',  to: 'users#new'
 
   resources :users
+  resources :microposts
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
